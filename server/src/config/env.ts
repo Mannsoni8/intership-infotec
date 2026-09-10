a321d62c93fe0bcfb636ee04444c1963e7826e13
Mannsoni8 <mannsoni4352@gmail.com>
2026-09-10 23:38:33 +0530

    feat: add PORT validation and include it in the config export

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -5,8 +5,13 @@ if (!process.env.MONGO_URI) {
     throw new Error("Database url is not define")
 }
 
+if (!process.env.PORT) {
+    throw new Error("Port is not define")
+}
+
 const config = {
-    MONGO_URI: process.env.MONGO_URI
+    MONGO_URI: process.env.MONGO_URI,
+    PORT: process.env.PORT
 }
 
 export default config
