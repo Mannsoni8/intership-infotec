31def6bcbe960855aec1a33357ae0055d04cf08b
Mannsoni8 <mannsoni4352@gmail.com>
2026-09-10 23:35:54 +0530

    feat: implement database connection using mongoose

---
+++ server/src/config/database.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose"
+import config from "./env"
+
+export const connectDD = async () => {
+    try {
+        await mongoose.connect(config.MONGO_URI)
+        console.log("Database is connected")
+    } catch (error) {
+        console.log(`Error in connecting database ${error}`)
+    }
+}
