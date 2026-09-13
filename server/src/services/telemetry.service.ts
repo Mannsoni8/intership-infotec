151bb8483df966ebfead8bd0e1a854c177e650f3
Mannsoni8 <mannsoni4352@gmail.com>
2026-09-13 22:44:12 +0530

    refactor: clean up getBucketStart function and improve addTelemetryReading logic

--- server/src/services/telemetry.service.ts
+++ server/src/services/telemetry.service.ts
@@ -1,9 +1,42 @@
+import { Types } from "mongoose";
+import { TelemetryBucket } from "../models/telemetry.model";
+import { ITelemetryReading } from "../types/telemetry.types";
+
+
 export const getBucketStart = (timestamp: Date): Date => {
-  const bucketStart = new Date(timestamp);
+    const bucketStart = new Date(timestamp);
 
-  bucketStart.setMinutes(0);
-  bucketStart.setSeconds(0);
-  bucketStart.setMilliseconds(0);
+    bucketStart.setMinutes(0);
+    bucketStart.setSeconds(0);
+    bucketStart.setMilliseconds(0);
 
-  return bucketStart;
+    return bucketStart;
+};
+
+
+export const addTelemetryReading = async (
+    vehicleId: Types.ObjectId,
+    reading: ITelemetryReading
+): Promise<void> => {
+    const bucketStart = getBucketStart(reading.timestamp);
+
+    await TelemetryBucket.findOneAndUpdate(
+        {
+            vehicleId,
+            bucketStart,
+        },
+        {
+            $push: {
+                readings: reading,
+            },
+
+            $inc: {
+                readingCount: 1,
+            },
+        },
+        {
+            upsert: true,
+            new: true,
+        }
+    );
 };
